b52c3e881ba8d975a2b5d3a3492b96693948a50c
DiegoAndre3666 <49461060+DiegoAndre3666@users.noreply.github.com>
2020-01-10 08:25:25 -0300

    Add files via upload

--- _topo.php
+++ _topo.php
@@ -1,5 +1,5 @@
 <div id="wrapper">
-    <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0">
+    <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0;background-color: #202020;">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
                 <span class="sr-only">Toggle navigation</span>
@@ -13,7 +13,7 @@
         <div style="color: white;
              padding: 15px 50px 5px 50px;
              float: right;
-             font-size: 16px;"> Departamento de TI - Granjeiro Alimentos &nbsp; <a href="login.php" class="btn btn-danger square-btn-adjust">Sair</a> </div>
+             font-size: 16px;"> Departamento de TI - Granjeiro Alimentos &nbsp; <a href="login.php" class="btn btn-danger ">Sair</a> </div>
 
 
     </nav>      
